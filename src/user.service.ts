be557691a90c8f181ac48060cb1c53412a49d32c
Ashwini  Zinjurde <71981911+ashwinizinjurde@users.noreply.github.com>
2024-12-12 14:35:59 +0000

    user.service.ts

---
+++ src/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'http://localhost:5000/register'; // Flask API URL
+
+  constructor(private http: HttpClient) {}
+
+  registerUser(user: User): Observable<any> {
+    return this.http.post(this.apiUrl, user);
+  }
+}
